f85baa8e8c0fcac7fdd550a2424977455fef4f2c
Nguyen Duong <nduongg04@gmail.com>
2024-11-17 11:51:33 +0700

    test cicd

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
 	getHello(): string {
-		return `Go to ${process.env.SWAGGER_URI} to see the API documentation`;
+		return `Hello World! Go to ${process.env.SWAGGER_URI} to see the API documentation`;
 	}
 }
